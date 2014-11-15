20fb0f8944e4022c41a638caee79c87e0640ec48
Sean Dion <smdion@gmail.com>
2014-11-15 17:25:54 -0600

    fix typos

--- ddclient/firstrun.sh
+++ ddclient/firstrun.sh
@@ -5,7 +5,6 @@ if [ -f /config/ddclient.conf ]; then
   echo "Using existing config file."
 else
   echo "Creating config from template."
-  wget -P /config/ https://raw.githubusercontent.com/lienma/UpsBoard/master/config.js-sample
-  mv /config/config.js-sample /config/config.js
-  chown nobody:users /config/config.js
+  wget -P /config/ https://raw.githubusercontent.com/smdion/docker-containers/master/ddclient/ddclient.conf
+  chown nobody:users /config/ddclient.conf
 fi
