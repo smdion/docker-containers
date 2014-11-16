06d12263ad22769cf5df1b685d7213907d7097d8
Sean Dion <smdion@gmail.com>
2014-11-16 13:03:41 -0600

    moved log creation

--- upstatsboard/firstrun.sh
+++ upstatsboard/firstrun.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Set log file to /config/logs
+ls -s /config/logs/debug.log /opt/UpsBoard-0.2.1/logs/debug.log
+
 # Check if config exists. If not, copy in the sample config
 if [ -f /config/config.js ]; then
   echo "Using existing config file."
@@ -10,13 +13,14 @@ else
   chown nobody:users /config/config.js
 fi
 
-# Check if logs exists. If not, copy in the sample config
+# Check if logs exists. If not, make blank
 if [ -f /config/logs/debug.log ]; then
   echo "Using existing log file."
 else
   echo "Creating log file"
 	if [[ ! -e /config/logs ]]; then
 		mkdir -p /config/logs
-		chown nobody:users /config/logs
+		cd /config/logs && touch debug.log
+		chown nobody:users /config/logs/debug.log
 	fi
 fi
