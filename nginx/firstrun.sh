fc3fe5feab6b1aaafe981049e05e8ca06b0f5292
Sean Dion <smdion@gmail.com>
2014-11-24 16:22:43 -0600

    Update firstrun.sh

--- nginx/firstrun.sh
+++ nginx/firstrun.sh
@@ -5,7 +5,7 @@ if [ -f /nginxconf/nginx.conf ]; then
   echo "Using existing config file."
 else
   echo "Creating config from template."
-  cp /etc/nginx/nginx.conf-temp /nginxconf/nginx.conf
+  cp /etc/nginx/nginx.conftemp /nginxconf/nginx.conf
   echo "\ndaemon off;" >> /nginxconf/nginx.conf
   chown nobody:users /nginxconf/nginx.conf
 fi
