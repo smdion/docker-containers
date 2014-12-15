a9e81bacb31b4bc57db1dff98b16610295730f71
Sean Dion <smdion@gmail.com>
2014-12-15 12:28:04 -0600

    Update firstrun.sh

--- DDNSnix/firstrun.sh
+++ DDNSnix/firstrun.sh
@@ -5,6 +5,6 @@ if [ -f /config/UnofficialDDNS.yaml ]; then
   echo "Using existing config file."
 else
   echo "Creating config from template."
-  mv /etc/UnofficialDDNS.yaml-dist /config/UnofficialDDNS.yaml
+  mv /etc/UnofficialDDNS-dist.yaml /config/UnofficialDDNS.yaml
   chown nobody:users /config/UnofficialDDNS.yaml
 fi
