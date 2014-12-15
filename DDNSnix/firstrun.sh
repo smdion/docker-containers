c894790d6b48f593d3f2064030b0a1e9bd0980c4
Sean Dion <smdion@gmail.com>
2014-12-15 12:14:32 -0600

    Update firstrun.sh

--- DDNSnix/firstrun.sh
+++ DDNSnix/firstrun.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#Set log file to /config/logs
-ls -s /config/UnofficialDDNS.yaml /etc/UnofficialDDNS.yaml
-
 # Check if config exists. If not, copy in the sample config
 if [ -f /config/UnofficialDDNS.yaml ]; then
   echo "Using existing config file."
