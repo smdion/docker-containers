a99725ab33fc74a3437a950471eb5cdceb189227
Sean Dion <smdion@gmail.com>
2014-12-16 23:31:50 -0600

    Update inotify.sh

--- reverseproxy/inotify.sh
+++ reverseproxy/inotify.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while inotifywait -e modify /config; do
-    runsv /etc/services/apache -k
-    runsv /etc/services/apache
+    runsv /etc/services/apache/ -k
+    runsv /etc/services/apache/
     echo "File in Config Folder Changed, Restarted"
 done
