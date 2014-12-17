a0ed9caaf18c60a494fd2e7eff8c6112031da986
Sean Dion <smdion@gmail.com>
2014-12-16 21:36:38 -0600

    Update inotify.sh

--- reverseproxy/inotify.sh
+++ reverseproxy/inotify.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 while inotifywait -e modify /config; do
-    killall -2 apache2
-    killall -2 httpd
-    exec /usr/sbin/apache2 -D FOREGROUND
+    apachectl -k stop
     echo "File in Config Folder Changed, Restarted"
 done
