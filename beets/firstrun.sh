822d1d5c7ef20c8ff10955924bec6d80991d495d
Sean Dion <smdion@gmail.com>
2014-12-27 11:56:53 -0600

    Update firstrun.sh

--- beets/firstrun.sh
+++ beets/firstrun.sh
@@ -3,7 +3,7 @@ if [ -f /config/beets.sh ]; then
   echo "Using existing config file."
 else
   echo "Creating beets.sh from template."
-  wget -P /config/ https://raw.githubusercontent.com/smdion/docker-containers/master/beets/src/beets.sh
+  wget -P /config https://raw.githubusercontent.com/smdion/docker-containers/master/beets/src/beets.sh
   chown nobody:users /config/beets.sh
   chmod +x beets.sh
 fi
