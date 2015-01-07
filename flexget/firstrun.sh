e07cd4148d05fb339fdf526f0f762a94b5dc0575
sparklyballs <sparklyballs@gmail.com>
2015-01-07 12:19:49 +0000

    fixed errors in firstrun.sh

--- flexget/firstrun.sh
+++ flexget/firstrun.sh
@@ -1,7 +1,12 @@
-if [ -f "/root/.flexget/config.yml" ]; then" ]; then
-echo "using existing config file"
+#!/bin/bash
 
+# Check if config exists. If not, copy in the sample config
+if [ -f /config/config.yml ]; then
+  echo "Using existing config file."
 else
-echo "creating blank config.yml"
-cp /config.yml /root/.flexget/
+  echo "Creating dunmmy config.yml."
+  cp /config.yml  /config/config.yml
 fi
+
+
+
