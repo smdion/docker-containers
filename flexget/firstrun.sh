b6389b25afb7a12a3dc2bb737885d8b13dd0436f
sparklyballs <sparklyballs@gmail.com>
2015-01-07 11:51:51 +0000

    Update firstrun.sh
    fix up config.yml path.

--- flexget/firstrun.sh
+++ flexget/firstrun.sh
@@ -1,7 +1,7 @@
-if [ -f "/config/config.yml" ]; then" ]; then
+if [ -f "/root/.flexget/config.yml" ]; then" ]; then
 echo "using existing config file"
 
 else
 echo "creating blank config.yml"
-cp /config.yml /config/config.yml
+cp /config.yml /root/.flexget/
 fi
