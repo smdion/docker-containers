7462e9a79b53de9be932c919f51e6a8f7ddccaf9
Sean Dion <smdion@gmail.com>
2015-01-17 13:35:53 -0600

    Update firstrun.sh

--- plexReport/firstrun.sh
+++ plexReport/firstrun.sh
@@ -8,3 +8,12 @@ else
   mv /opt/plexReport/etc/config.yaml.example /config/config.yaml
   chown nobody:users /config/config.yaml
 fi
+
+# Check if crons.conf exists. If not, copy in the sample config
+if [ -f /config/crons.conf ]; then
+  echo "Using existing config file."
+else
+  echo "Creating config from template."
+  cp /root/crons.conf /config/crons.conf
+  chown nobody:users /config/crons.conf
+fi
