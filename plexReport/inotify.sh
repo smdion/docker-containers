446a8a8063896ade04598e6e936ba79c31a44daf
Sean Dion <smdion@gmail.com>
2015-01-17 13:32:53 -0600

    Create inotify.sh

---
+++ plexReport/inotify.sh
@@ -0,0 +1,9 @@
+   #!/bin/sh
+
+while inotifywait -e modify /config/crons.conf; do
+    rm /root/crons.conf
+    cp /config/crons.conf /root/crons.conf
+    crontab /root/crons.conf
+    cron
+    echo "Changed cron"
+done
