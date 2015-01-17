18fb9aa9a1c8fcfb132a0f1200e47ed24d04644b
Sean Dion <smdion@gmail.com>
2015-01-17 14:26:00 -0600

    Update inotify.sh

--- plexReport/inotify.sh
+++ plexReport/inotify.sh
@@ -2,6 +2,7 @@
 
 while inotifywait -e modify /config --exclude config.yaml; do
     cron stop
+    rm /var/run/crond.pid
     rm /root/crons.conf
     cp /config/crons.conf /root/crons.conf
     crontab /root/crons.conf
