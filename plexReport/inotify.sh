5498dcf4bd34e7d96acb956d32aeb614cccf7513
Sean Dion <smdion@gmail.com>
2015-06-10 14:58:25 -0500

    Delete inotify.sh

--- plexReport/inotify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-while inotifywait -e modify /config --exclude config.yaml; do
-    rm /root/crons.conf
-    sleep 5
-    cp /config/crons.conf /root/crons.conf
-    crontab /root/crons.conf
-    echo "Changed cron"
-done
